86bb0f8fca2bec9bdd04f561b3caa6824d028e20
Michael Klein <michael.klein.business@gmail.com>
2021-02-09 07:05:19 +0100

    version bump

--- dist/components/sidebar_link.d.ts
+++ dist/components/sidebar_link.d.ts
@@ -1,5 +1,6 @@
 import { h } from "preact";
-import { NavbarItem } from "../utils/document_interfaces";
+import { DocumentHeading, NavbarItem } from "../utils/document_interfaces";
 export declare const SidebarLink: (props: {
     item: NavbarItem;
+    heading?: DocumentHeading;
 }) => h.JSX.Element;
